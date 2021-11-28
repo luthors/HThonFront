73f4ba7be8c7cf5da217349776d473726fbf8722
luthors <luis.g.toro@gmail.com>
2021-11-28 09:53:40 -0500

    1st Front

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  Menues from './components/Menues';
+
+import logo from './logo.svg';
+
+
+
+function App() {
+  return (
+    <div className="bg-gray-50">
+      <Menues />
+    </div>
+  );
+}
+
+export default App;
